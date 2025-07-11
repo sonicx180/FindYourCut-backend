83448f142835e3184b35606ddbc9e435407f975e
Sonicx180 <83236320+sonicx180@users.noreply.github.com>
2025-07-11 12:55:22 -0700

    port fix

--- src/index.ts
+++ src/index.ts
@@ -68,7 +68,7 @@ app.get('/gallery', async (c) => {
 serve(
 	{
 		fetch: app.fetch,
-		port: 3000,
+		port: 45827,
 	},
 	(info) => {
 		console.log(`Server is running on http://localhost:${info.port}`);
